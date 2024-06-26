957b6e8722e0b97d73e93e1513b34908d91c7d5d
ViktoR <Kachamakov@abv.bg>
2024-06-26 20:07:21 +0300

    CSS changed

--- SignalRChat/wwwroot/js/Chat.js
+++ SignalRChat/wwwroot/js/Chat.js
@@ -4,12 +4,12 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 document.getElementById("sendButton").disabled = true;
 
-connection.on("ReceiveMessage", function (user, message) {
+connection.on("ReceiveMessage", function (message) {
 
     var li = document.createElement("li");
     document.getElementById("messageList").appendChild(li);
 
-    li.textContent = `${user} says ${message}`;
+    li.textContent = `${message}`;
 
 });
 
@@ -23,11 +23,12 @@ connection.start().then(function () {
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
 
-    var user = document.getElementById("userInput").value;
+   
     var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+    connection.invoke("SendMessage", message).catch(function (err) {
 
         return cosole.error(err.toString());
     });
     event.preventDefault();
 });
+
